42a710efc3e1a080665d2da9dcddec0623d18cc3
Hexagram <lmx_Hexagram@outlook.com>
2020-01-25 13:26:00 +0800

    修正了图片接口的问题

--- main_code/back/src/imageSystem.go
+++ main_code/back/src/imageSystem.go
@@ -14,12 +14,13 @@ func (s *Service) getImage(c *gin.Context) {
 	//fmt.Println(sum)
 
 	rand.Seed(time.Now().Unix())
-	tempID := rand.Intn(sum) + 1
+	tempID := rand.Intn(sum-1)+1
 
 	//fmt.Println(tempID)
 
 	tempImageURL := new(imageURL)
 	s.DB.Table("image_urls").Where("id = ?",tempID).Find(tempImageURL)
+	//fmt.Println(tempImageURL)
 	c.Redirect(http.StatusFound,tempImageURL.URL)
 	//return makeSuccessReturn(200,gin.H{"URL":tempImageURL.URL,"created_at":tempImageURL.CreatedAt})
 }
